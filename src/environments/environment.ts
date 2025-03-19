7bf14e5324b6c56785880243df49d17d66e6907d
Omdevsinh-Zala <omdevsinh@krishaweb.com>
2025-03-19 14:17:56 +0530

    Changes in env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
-    production: false,
-    firebaseConfig: {
-      apiKey: 'your-api-key',
-      authDomain: 'your-auth-domain',
-      projectId: 'your-project-id',
-      storageBucket: 'your-storage-bucket',
-      databaseURL: 'your-database-url',
-      messagingSenderId: 'your-messaging-sender-id',
-      appId: 'your-app-id',
-      measurementId: 'your-measurement-id',
-    },
-    vapidKey: 'your-vapid-key',
-    santryDNS: 'your-website-dns',
-    vercelSite: 'your-website-url'
-  };
+  production: false,
+  firebaseConfig: {
+    apiKey: 'your-api-key',
+    authDomain: 'your-auth-domain',
+    projectId: 'your-project-id',
+    storageBucket: 'your-storage-bucket',
+    databaseURL: 'your-database-url',
+    messagingSenderId: 'your-messaging-sender-id',
+    appId: 'your-app-id',
+    measurementId: 'your-measurement-id',
+  },
+  vapidKey: 'your-vapid-key',
+  santryDNS: 'your-website-dns',
+  vercelSite: 'your-website-url'
+};
