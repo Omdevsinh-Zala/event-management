e34fc4a0eccbffb2947ce7b87ae0c609721b0f8d
Omdevsinh-Zala <omdevsinh@krishaweb.com>
2025-03-25 17:02:44 +0530

    Improve overall ui, fix minor bugs and improve details page

--- src/app/detail/detail.component.spec.ts
+++ src/app/detail/detail.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailComponent } from './detail.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../../environments/environment.prod';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+
+jest.mock('@angular/fire/database', () => ({
+  provideDatabase: jest.fn(),
+  getDatabase: jest.fn()
+}))
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
@@ -8,6 +18,11 @@ describe('DetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers:[
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore())
+      ],
       imports: [DetailComponent]
     })
     .compileComponents();
